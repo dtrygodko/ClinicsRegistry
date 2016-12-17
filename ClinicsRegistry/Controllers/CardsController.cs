bb40d274f0bb27e2ac6f9ac2c82fa34d3f78ef59
dtrygodko <dtrygodko@ukr.net>
2016-12-17 19:19:51 +0200

    Added DI, new functionality

--- ClinicsRegistry/Controllers/CardsController.cs
+++ ClinicsRegistry/Controllers/CardsController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ClinicsRegistry.Models;
 
 namespace ClinicsRegistry.Controllers
 {
     public class CardsController : Controller
     {
+        RegistryDBContext _context;
+
+        public CardsController(RegistryDBContext context)
+        {
+            _context = context;
+        }
+
         // GET: Cards
         public ActionResult Index()
         {
@@ -20,13 +28,29 @@ namespace ClinicsRegistry.Controllers
             return View();
         }
 
+        // GET: Cards/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
         // POST: Cards/Create
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
             try
             {
-                // TODO: Add insert logic here
+                ClientCard card = new ClientCard
+                {
+                    Id = new Guid(),
+                    Name = collection.Get("Name"),
+                    Surname = collection.Get("Surname"),
+                    BirthDate = DateTime.Parse(collection.Get("BirthDate")),
+                    IsEmployee = bool.Parse(collection.Get("IsEmployee"))
+                };
+
+                _context.Cards.Add(card);
+                _context.SaveChanges();
 
                 return RedirectToAction("Index");
             }
@@ -34,6 +58,12 @@ namespace ClinicsRegistry.Controllers
             {
                 return View();
             }
+        }
+
+        // GET: Cards/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            return View();
         }
 
         // POST: Cards/Edit/5
